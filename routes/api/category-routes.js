98e5f195f94dbb0e978dc0c90dd11f38bc2a03fb
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-01 11:12:51 -0400

    updating routes for id param

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,10 +16,17 @@ router.get('/', (req, res) => {
   }
 }); 
 
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
+// find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', (req, res) => {
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
