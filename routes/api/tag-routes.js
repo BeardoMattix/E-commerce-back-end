6f2d989e5a1c7acebac74687c5d8964f7a21c82e
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-06 09:55:21 -0400

    updating index in models folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,8 +64,23 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-// router.delete('/:id', (req, res) => {
-//   // delete on tag by its `id` value
-// });
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteTag) {
+      res.status(404).json({ message: "No Tag found with that id!" });
+      return;
+    }
+
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
