42f6eb5021dcd69105e5c4e67e76dae867f45fa7
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-07 13:49:07 -0400

    fixing bug in tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Product.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     if (!tagData) {
